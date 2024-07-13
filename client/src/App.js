1757a2084ca71e59b7943bb2f52787c0060d2946
Yash020405 <yashaga0204@gmail.com>
2024-07-13 10:52:35 +0530

    updated_app

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import ProtectedRoute from "./components/protectedRoute";
 
 
 function App() {
-
-
   return (
     <div className="App">
      
@@ -16,12 +15,11 @@ function App() {
     
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/" element={<Home/>} />
+          <Route path="/" element={<ProtectedRoute><Home/></ProtectedRoute>} />
          
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
-
 export default App;
